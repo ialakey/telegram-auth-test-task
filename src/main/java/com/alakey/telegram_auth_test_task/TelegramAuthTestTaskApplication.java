0a295a1fd6c66a6bc3537f834bb7821856c8021b
Ilya <prosoulk2017@gmail.com>
2025-05-25 19:18:33 +0200

    init project

---
+++ src/main/java/com/alakey/telegram_auth_test_task/TelegramAuthTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.alakey.telegram_auth_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramAuthTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramAuthTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alakey/telegram_auth_test_task/TelegramAuthTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alakey.telegram_auth_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramAuthTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
